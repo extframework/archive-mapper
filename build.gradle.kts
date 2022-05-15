27c8eaa57ce443187c13b33511e4e9d0059445ca
Durgan <durganmcbroom@gmail.com>
2022-05-15 10:14:05 -0700

    Added publishing info.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     kotlin("jvm") version "1.6.21"
+
+    id("signing")
+    id("maven-publish")
+    id("org.jetbrains.dokka") version "1.6.21"
 }
 
 group = "net.yakclient"
@@ -44,4 +48,74 @@ tasks.test {
 tasks.compileJava {
     targetCompatibility = "17"
     sourceCompatibility = "17"
+}
+
+task<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+task<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaJavadoc)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("archive-mapper-maven") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            artifactId = "archive-mapper"
+
+            pom {
+                name.set("Archive Mapper")
+                description.set("A mapping parser for de-obfuscation mappings(proguard)")
+                url.set("https://github.com/yakclient/archive-mapper")
+
+                packaging = "jar"
+
+                developers {
+                    developer {
+                        id.set("Chestly")
+                        name.set("Durgan McBroom")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("GNU General Public License")
+                        url.set("https://opensource.org/licenses/gpl-license")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/yakclient/archive-mapper")
+                    developerConnection.set("scm:git:ssh://github.com:yakclient/archive-mapper.git")
+                    url.set("https://github.com/yakclient/archive-mapper")
+                }
+            }
+        }
+    }
+
+    repositories {
+        if (!project.hasProperty("maven-user") || !project.hasProperty("maven-pass")) return@repositories
+
+        maven {
+            val repo = if (project.findProperty("isSnapshot") == "true") "snapshots" else "releases"
+
+            isAllowInsecureProtocol = true
+
+            url = uri("http://repo.yakclient.net/$repo")
+
+            credentials {
+                username = project.findProperty("maven-user") as String
+                password = project.findProperty("maven-pass") as String
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
 }
